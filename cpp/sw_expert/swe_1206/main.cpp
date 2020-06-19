5bc10a1fb3bdad47cb9894b2f16cb90e14f8f342
CH Yang <ych1273@gmail.com>
2020-06-19 01:03:34 +0000

    1206 commit!

--- cpp/sw_expert/swe_1206/main.cpp
+++ cpp/sw_expert/swe_1206/main.cpp
@@ -12,28 +12,59 @@ int main(int argc, char** argv)
 	for(test_case=1; test_case<=T; ++test_case)
 	{
 		int count, horizon = 0;
-		int height[1000] = {0};
+		int height[5] = {0};
+		int index = 2;
 		
 		cin >> horizon;
 		count = 0;
-		//cout << "horizon:" << horizon << " ";
-		for(int i = 0; i < horizon; i++)
+		
+		for(int i = 0; i < 5; i++)
 		{
 			cin >> height[i];
 		}
-		
-		for(int i = 2; i < horizon - 2; i++)
+
+		for(int i = 2; i < horizon-2; i++)
 		{
-			int a = 0;
-			a = max(height[i-2], height[i-1]);
-			a = max(a, height[i+1]);
-			a = max(a, height[i+2]);
-			
-			if(height[i] > a)
+			int max = 0;
+			bool ismax = false;
+
+			max = 0;
+			for(int j = 0; j < 5; j++)
 			{
-				count += height[i]-a;
-				//cout << i << "count:" <<count << " ";
+				if(j != index)
+				{
+					if(max < height[j])
+					{
+						max = height[j];
+					}
+					
+					if(height[index] <= height[j])
+					{
+						ismax = false;
+						break;
+					}
+				}
+				ismax = true;
 			}
+			
+			if(ismax == true)
+			{
+				count += height[index]-max;
+			}
+			else
+			{
+			}
+			
+			if(index < 2)
+			{
+				if(i != horizon-3) cin >> height[index+3];
+			}
+			else{
+				if(i != horizon-3) cin >> height[index-2];
+			}
+
+			index++;
+			if(index > 4) index = 0;
 		}
 	
 		cout << "#" << test_case << " " << count << "\n";
