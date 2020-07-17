f9e904a943237eb4bf51518cc41e04320b893878
CH Yang <ych1273@gmail.com>
2020-07-17 13:30:08 +0000

    아이 얼마 못했네 1868

--- cpp/sw_expert/1868/main.cpp
+++ cpp/sw_expert/1868/main.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 char nxn[300][300];
+int nxn_boom[300][300];
+int dx[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+int dy[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
 
 int main(int argc, char** argv)
 {
@@ -17,15 +20,65 @@ int main(int argc, char** argv)
 	{
 		int N = 0;
 		cin >> N;
-		
+		//cout << N << "!!!";
 		//test_case 값을 먼저 받음.
 		for(int i = 0; i < N; i++)
 		{
 			for(int j = 0; j < N; j++)
 			{
-				cin >> &nxn[i][j];
+				nxn[i][j] = '\0';
+				while(nxn[i][j] != '*' && nxn[i][j] != '.')
+				{
+					scanf("%c", &nxn[i][j]);
+				}
 			}
 		}
+		int boom_cnt = 0;
+		for(int i = 0; i < N; i++)
+		{
+			for(int j = 0; j < N; j++)
+			{
+				if(nxn[i][j] == '*')
+				{
+					boom_cnt = 9;
+				}
+				else
+				{
+					for(int xx = 0; xx < 8; xx++)
+					{
+						int nx = i + dx[xx];
+						int ny = j + dy[xx];
+						
+						
+						//cout << i << "," << j << ":" << nx << "," << ny << "\n";
+						if( (nx >= 0) && (nx < N) && (ny >= 0) && (ny < N) )
+						{
+							//cout << "!";
+							if(nxn[nx][ny] == '*')
+							{
+								cout << i << "." << j << ":" << nx << "." << ny << "\n";
+								boom_cnt++;
+							}
+						}
+					}
+				}
+				
+				nxn_boom[i][j] = boom_cnt;
+				boom_cnt = 0;
+			}
+		}
+		
+		#if 1
+		for(int i = 0; i < N; i++)
+		{
+			for(int j = 0; j < N; j++)
+			{
+				cout << nxn_boom[i][j] << " ";
+			}
+			cout << "\n";
+		}
+		#endif
+		
 		
 		//cout << "#" << test_case << " " << round(sum) << "\n";
 	}
