3d6bd719244fd2d430da7b77a8d08fb122f1a13d
CH Yang <ych1273@gmail.com>
2020-07-19 12:04:55 +0000

    lmake.sh 수정

--- cpp/lmake.sh
+++ cpp/lmake.sh
@@ -2,6 +2,7 @@
 #echo "파라미터 개수 : $#"
 #echo "첫 번째 파라미터 : $1"
 #echo "모든 파라미터 내용 : $@"
+CTARGET="1868"
 
-rm ./$1.exe
-make SRC=$1 OSENV=LINUXOS
+rm ./${CTARGET}.exe
+make SRC=${CTARGET} OSENV=LINUXOS
